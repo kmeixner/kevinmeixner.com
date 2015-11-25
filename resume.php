a465da0d41caeb651ed29f84ecf6c73a890e2f31
Kevin Meixner <kevin@slyce.it>
2015-11-25 10:08:03 -0500

    Removed HTML & text only resume until I have time to update them
    modified:   resume.php

--- resume.php
+++ resume.php
@@ -18,8 +18,8 @@
 	
 	<a href="resume/kevinmeixner_seniordeveloper_resume.doc" data-role="button" data-ajax="false">MS Word 97-2003</a>
 	<a href="resume/kevinmeixner_seniordeveloper_resume.pdf" data-role="button" data-ajax="false">Adobe PDF</a>
-	<a href="resume/kevinmeixner_seniordeveloper_resume.txt" data-role="button" data-ajax="false">Plain Text (right-click and choose Save As...)</a>
-	<a href="resume_html.php" data-role="button" data-ajax="false" target="_blank">Plain HTML</a>
+	<!--a href="resume/kevinmeixner_seniordeveloper_resume.txt" data-role="button" data-ajax="false">Plain Text (right-click and choose Save As...)</a>
+	<a href="resume_html.php" data-role="button" data-ajax="false" target="_blank">Plain HTML</a-->
   
   </div><!-- /content -->
 
