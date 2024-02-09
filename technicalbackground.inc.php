b09eff1663f7edc1c05af4a84e086dd32976a851
Kevin Meixner <kmeixner42@gmail.com>
2024-02-09 11:37:47 -0500

    new file:   technicalbackground.inc.php

---
+++ technicalbackground.inc.php
@@ -0,0 +1,27 @@
+	<p>
+	I am a Senior Software Developer with 20+ years of experience helping startups and small to midsized businesses fill in the gaps in their teams and push cutting-edge technology forward. If you have an out-of-the-box idea to implement, I am your man. 
+	</p>
+	
+	<p>
+	I am most experienced with PHP, SQL, JavaScript, AWS, MySQL, PostgreSQL, Linux, LAMP and SQL Server. I have also worked on projects in Laravel, Ruby on Rails, Vue.js, NodeJS, Python, C++, and Java. I have been working on the full stack for most of my work experience. I am also strong in relational database design. I am experienced at learning new emerging technologies on the job quickly.
+	</p>
+	
+	<p>
+	What sets me apart from most other developers are my business and interpersonal communication skills. I am happy to interact with users and stakeholders at all levels: blue-collar factory workers, white-collar office workers, CEOs and company owners, to gather requirements and develop project scope. I am happy to write business documents, technical documents, and user manuals written at a level the intended audience can understand. I also have experience leading presentations and group training sessions.
+	</p>
+	
+	<p>
+	On most past jobs, I used critical thinking and judgment to make design decisions about the user interface (UI) to make the user experience (UX) intuitive and user-friendly. I am experienced at gathering requirements from clients, users and stakeholders and translating those into a product scope and technical project scope before implementing the project. Once the requirements are gathered, I am typically the one doing the hands-on implementation myself or assisting hands-on with a team of other developers to create the software code.
+	</p>
+	
+	<p>
+	I thrive most when working in innovative, agile, fast-paced, open, and collaborative work environments. I have worked 10+ years for startup companies creating bleeding-edge software that pushed the limits of what was possible to do with the current technology of the time. My variety of experience makes me a "jack-of-all-trades" since I am used to "wearing many hats" in fast-growing startups, small companies and midsized companies.
+	</p>
+
+	<p>
+	I have a strong personal interest in working with companies or organizations that are helping people grow and become better people and/or enriching the lives of people in a positive way and/or doing good things in the world.
+	</p>
+	
+	<p>
+	For a detailed list of my work experience, please visit my <a href="http://linkedin.com/in/kevinmeixner/" target="_blank">LinkedIn Profile</a>
+	</p>
